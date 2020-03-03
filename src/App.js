c8688ca2632d9ba737128229e8c3841292ba94dd
Vele Tosevski <vele97@gmail.com>
2020-03-03 13:37:33 -0500

    added url parser for table_id

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React from "react";
 import { HoverMorphIcon } from "react-svg-buttons";
+import qs from "qs";
+import assert from "assert";
 
 import "./App.css";
+import { findByLabelText } from "@testing-library/react";
+
+function isPositiveInteger(n) {
+  return n >>> 0 === parseFloat(n);
+}
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    var prefixed = qs.parse(this.props.location.search, { ignoreQueryPrefix: true });
+    console.log(prefixed);
+
+    var table_id;
+    try {
+      if (isPositiveInteger(prefixed.table_id)) {
+        table_id = parseInt(prefixed.table_id);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+
+    this.state = {
+      table_id: table_id
+    };
+  }
+
+  componentDidMount() {}
+
   render() {
     return (
       <div className="AppContainer">
@@ -17,6 +46,7 @@ class App extends React.Component {
               color="#dd6e78"
             />
           </div>
+          <span style={{ alignSelf: "center" }}>Table ID = {this.state.table_id}</span>
         </div>
       </div>
     );
